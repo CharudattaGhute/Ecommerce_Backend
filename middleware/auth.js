7f65167a0cc6d261fdbfd9fdb3226e632856e612
CharudattaGhute <ghutecharudatta@gmail.com>
2024-08-22 23:06:13 +0530

    auth updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,36 @@
 const jwt = require("jsonwebtoken");
 
-const authorise = (req, res, next) => {
+exports.auth = (req, res, next) => {
   const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+
   const bearerword = token.split(" ")[0].trim();
   const bearertoken = token.split(" ")[1];
-  if (bearerword != "Bearer") {
-    return res.status(401).send("Acess denied.Invalid token");
+
+  if (bearerword !== "Bearer") {
+    return res.status(401).send("Access denied. Invalid token format.");
   }
-  if (!bearertoken) {
-    return res.status(401).send("Acess denied. No token Provided");
-  }
+
   try {
     const decoded = jwt.verify(bearertoken, "key");
-    req.user = decoded.userId;
+    req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).send("Invalid token");
+    res.status(401).send("Invalid token.");
   }
 };
 
-module.exports = authorise;
+exports.admin = (req, res, next) => {
+  if (!req.user || !req.user.role) {
+    return res.status(403).send({ msg: "Access Denied! No user role found." });
+  }
+
+  if (req.user.role !== "admin") {
+    return res.status(403).send({ msg: "Access Denied! Admins only." });
+  }
+
+  next();
+};
