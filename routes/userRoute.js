cec7e317f4b363ac60dbc77e258755db27335623
CharudattaGhute <ghutecharudatta@gmail.com>
2024-08-26 16:09:03 +0530

    useeriuter.js updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const usercontroller = require("../controller/usercontroller");
+const authorise = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/register", usercontroller.register);
 router.post("/login", usercontroller.login);
+router.post("/userinfo", authorise.auth, usercontroller.userinfo);
 
 module.exports = router;
