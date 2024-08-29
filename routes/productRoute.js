4199b79726d83f33ddcee3e4cf75383dfbbdde73
CharudattaGhute <ghutecharudatta@gmail.com>
2024-08-29 19:52:19 +0530

    product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,8 +4,15 @@ const authorise = require("../middleware/auth");
 const router = express.Router();
 
 const productcontroller = require("../controller/productcontroller");
+const upload = require("../middleware/multer");
 
-router.post("/createproduct", productcontroller.createproduct);
+router.post(
+  "/createproduct",
+  authorise.auth,
+  authorise.admin,
+  upload.single("image"),
+  productcontroller.createproduct
+);
 router.get("/getallproduct", authorise.auth, productcontroller.getallproduct);
 router.get("/getproductbyid/:id", productcontroller.getproductbyid);
 router.put("/updateproduct/:id", productcontroller.updateproduct);
