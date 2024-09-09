79f787441f8e2e2aa279dffc00581d223291ddb5
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-09 19:05:10 +0530

    multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,17 +1,15 @@
 const multer = require("multer");
 const path = require("path");
 
-// Define storage location and filename format
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Directory where files will be stored
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-// File filter to accept only certain types of files
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
   if (allowedTypes.includes(file.mimetype)) {
